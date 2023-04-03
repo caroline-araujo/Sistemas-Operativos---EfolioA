0951f146df0eeb54a3254c27e8c1d9c4acc3d4ab
Caroline Araujo <carolinearaujo570@gmail.com>
2023-04-03 22:51:03 +0100

    adicionando o processo B

--- gsn.c
+++ gsn.c
@@ -8,22 +8,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
 
 int main(int argc, char *argv[]) {
     
-    //Aqui checamos se temos pelo menos um argumento, além do nome do programa. Se não tiver, o programa termina. Também checamos se o número de argumento é maior que três, uma vez que só queremos que seja fornecido o nome do programa + 1 argumento (número de bytes). Se fornecer um argumento a mais, o programa apresenta erro de execução e termina. 
-    if (argc < 2 || argc >= 3 ) { 
+    //Aqui checamos se temos os dois argumentos (nome do programa e número de bytes). Se não tiver exatamente 2, o programa termina. 
+    if (argc != 2) { 
         printf("Erro na execução do programa.\n");
         exit(1);
-    } else { //Checamos se o argumento fornecido (número aleatório de bytes) é diferente ou maior que zero. Se for igual ou menor que zero, o programa termina. Se for maior, o programa continua.
+    } else { 
         int n = atoi(argv[1]); //Aqui, precisamos converter o vetor argv[1], que é uma string, para um inteiro para que possamos comparar com zero. Para isso, utilizamos a função da stdlib atoi(). 
         
-        if (n <= 0) {
+        if (n <= 0) { //Checamos se o argumento fornecido (número aleatório de bytes) é diferente ou maior que zero. Se for igual ou menor que zero, o programa termina. Se for maior, o programa continua.
             printf("Erro: n <= 0!\n");
             exit(1);
         } else {
-            printf("O argumento fornecido é: %s\n", argv[1]);
-            return 0;
+            printf("O argumento fornecido é: %d\n", n);
+            int pid = fork();
+
+                if (pid > 0) {
+                    // Código do processo pai
+                    printf("Processo A: PID=%d  PPID=%d\n", getpid(), getppid());
+                    wait(NULL);
+                } else if (pid == 0) {
+                    // Código do processo filho
+                    printf("Processo B: PID=%d PPID=%d\n", getpid(), getppid());
+
+                    // Redirecionando a saída padrão para o arquivo "tmp.bin"
+                    FILE* saida = freopen("tmp.bin", "a", stdout);
+
+                    // Verificando se a abertura do arquivo foi bem sucedida
+                    if (saida == NULL) {
+                        printf("Erro ao abrir o arquivo tmp.bin. Fim do programa!\n");
+                        exit(1);
+                    }
+
+                    // Usando head para gerar a lista de bytes no arquivo
+                    execlp("head", "head", "-c", argv[1], "/dev/random", NULL);
+
+                    // Lendo o conteúdo do arquivo e imprimindo na tela
+                    char buffer[n];
+                    fread(buffer, sizeof(char), n, saida);
+                    for (int i = 0; i < n; i++) {
+                        printf("%d ", buffer[i]);
+                    }
+                    printf("\n");
+
+                     // Fechando o arquivo
+                    fclose(saida);
+
+                    // A linha abaixo só será executada se houver um erro na chamada do exec
+                    printf("Erro no exec. Fim do programa!\n");
+                    exit(1);
+                } else {
+                    printf("Erro no exec: %s. Fim do programa!\n", strerror(errno));
+                    exit(1);
+                }
         }
     }
     return 0;
